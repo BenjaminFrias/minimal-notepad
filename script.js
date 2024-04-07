15c8ef2acc47418dcc934c6a4fd00b92aa249b30
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-04-06 22:21:46 -0400

    add icon

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const textArea = document.querySelector("textarea");
 const arrowIcon = document.querySelector(".arrow");
+const nav = document.querySelector("nav");
+let navExpanded = nav.getAttribute("aria-expanded");
+let arrowExpanded = arrowIcon.getAttribute("aria-expanded");
+console.log("first: ", navExpanded);
 
 arrowIcon.addEventListener("click", () => {
-})
+    if (!navExpanded) {
+        navExpanded = true;
+    }
+    else {
+        navExpanded = false;
+    }
+    console.log(navExpanded);
+    arrowExpanded = arrowExpanded ? false : true;
+    nav.setAttribute("aria-expanded", navExpanded);
+    arrowIcon.setAttribute("aria-expanded", arrowExpanded);
+});
 
 textArea.focus();
