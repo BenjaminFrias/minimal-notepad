25d4ba9fc632e5c8cc335c654382546a9dcee3e1
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-04-12 19:11:47 -0400

    add icons in nav

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const textArea = document.querySelector("textarea");
-const arrowIcon = document.querySelector(".arrow");
 const nav = document.querySelector("nav");
-let navExpanded = nav.getAttribute("aria-expanded");
-let arrowExpanded = arrowIcon.getAttribute("aria-expanded");
-console.log("first: ", navExpanded);
-
-arrowIcon.addEventListener("click", () => {
-    if (!navExpanded) {
-        navExpanded = true;
-    }
-    else {
-        navExpanded = false;
-    }
-    console.log(navExpanded);
-    arrowExpanded = arrowExpanded ? false : true;
-    nav.setAttribute("aria-expanded", navExpanded);
-    arrowIcon.setAttribute("aria-expanded", arrowExpanded);
-});
+const arrowIcon = document.querySelector(".arrow");
+const wordBtn = document.querySelector(".word-limit-btn");
+const timeBtn = document.querySelector(".time-limit-btn");
+const widthBtn = document.querySelector(".full-width-btn");
+const fontBtn = document.querySelector(".change-font-btn");
+const lockBtn = document.querySelector(".lock-btn");
+const saveBtn = document.querySelector(".save-btn");
 
 textArea.focus();
+
+arrowIcon.addEventListener("click", () => {
+    arrowIcon.classList.toggle("expanded");
+    nav.classList.toggle("expanded");
+});
+
+
+wordBtn.addEventListener("click", () => {
+    words = +prompt("How much words do you want to write?", 0)
+    console.log(words)
+})
