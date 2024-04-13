8142a19f06ec165f51d40aca6f2b6a7e43233b5f
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-04-12 20:45:29 -0400

    Add limit input

--- script.js
+++ script.js
@@ -7,16 +7,46 @@ const widthBtn = document.querySelector(".full-width-btn");
 const fontBtn = document.querySelector(".change-font-btn");
 const lockBtn = document.querySelector(".lock-btn");
 const saveBtn = document.querySelector(".save-btn");
+const limitContainer = document.querySelector(".input-container");
+const limitText = document.querySelector(".input-container p");
+const limitInput = limitContainer.querySelector("#limit-input");
+const submitBtn = limitContainer.querySelector("#submit-input");
+
+let wordCount = 0;
+let wordLimit = 0;
+let timeLimit = 0;
 
 textArea.focus();
+
+textArea.addEventListener("click", () => {
+    limitContainer.classList.remove("expanded");
+    arrowIcon.classList.remove("expanded");
+    nav.classList.remove("expanded");
+});
+
+textArea.addEventListener("keyup", (key) => {
+    for (let i = 0; i < 1; i++) {
+        if (key.key == " ") {
+            wordCount += 1;
+        }
+    }
+});
 
 arrowIcon.addEventListener("click", () => {
     arrowIcon.classList.toggle("expanded");
     nav.classList.toggle("expanded");
+
+    if (!arrowIcon.classList.contains("expanded")) {
+        limitContainer.classList.remove("expanded");
+    }
 });
 
-
 wordBtn.addEventListener("click", () => {
-    words = +prompt("How much words do you want to write?", 0)
-    console.log(words)
-})
+    limitContainer.classList.toggle("expanded");
+    limitText.innerHTML = "Word limit:";
+});
+
+submitBtn.addEventListener("click", () => {
+    limitContainer.classList.toggle("expanded");
+    wordLimit = limitInput.value;
+});
